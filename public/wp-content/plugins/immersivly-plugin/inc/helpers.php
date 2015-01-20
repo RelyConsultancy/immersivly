247068a845d04d4499ac9f8ed3d9bc67c9a35e15
Stan Laurentiu <laurentiu.stan@freshbyteinc.com>
2015-01-20 13:01:03 +0200

    5-saveforlater: Add basic documentation to the functions.

--- public/wp-content/plugins/immersivly-plugin/inc/helpers.php
+++ public/wp-content/plugins/immersivly-plugin/inc/helpers.php
@@ -1,5 +1,8 @@
 <?php
 /**
+ * Check if post was already saved by the user, by supplying the user id and
+ * post id.
+ *
  * @param $user_id integer
  *  The internal Word Press user ID.
  * @param $post_id integer
@@ -21,6 +24,16 @@ function immersivly_check_if_post_is_not_saved($user_id, $post_id) {
 	return empty($result) ? FALSE : TRUE;
 }
 
+/**
+ * Get all the saved posts for a supplied user ID.
+ *
+ * @param $user_id integer
+ *  The internal Word Press user ID.
+ *
+ * @return object | bool
+ *  The list of saved posts.
+ *  No saved posts were found.
+ */
 function immersivly_retrieve_post_for_user($user_id) {
 	// Initialize the Word Press database wrapper.
 	global $wpdb;
@@ -29,6 +42,6 @@ function immersivly_retrieve_post_for_user($user_id) {
 	// Check if the user with the supplied user ID has already "Saved for later"
 	// the post with the supplied ID.
 	$results = $wpdb->get_results( "SELECT * FROM {$table_name} WHERE user_id = {$user_id} ", OBJECT );
-
+	// Return the list of saved posts. False otherwise.
 	return !empty($results) ? $results : FALSE;
 }
