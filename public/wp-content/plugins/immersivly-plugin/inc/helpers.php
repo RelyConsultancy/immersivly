d1cedd61289974555c9d3979405e62a922817c72
Stan Laurentiu <laurentiu.stan@freshbyteinc.com>
2015-01-22 15:55:49 +0200

    34-socialshares: Add the social share update functionality.
    https://trello.com/c/cMJdL0RX/34-social-media-shares

--- public/wp-content/plugins/immersivly-plugin/inc/helpers.php
+++ public/wp-content/plugins/immersivly-plugin/inc/helpers.php
@@ -45,3 +45,36 @@ function immersivly_retrieve_post_for_user($user_id) {
 	// Return the list of saved posts. False otherwise.
 	return !empty($results) ? $results : FALSE;
 }
+
+/**
+ * Get the number of shares for a supplied post ID.
+ *
+ * @param $post_id integer
+ *  The id for the post.
+ *
+ * @return int
+ *  The number of shares.
+ */
+function immersivly_get_the_number_of_shares($post_id) {
+	// Initialize the Word Press database wrapper.
+	global $wpdb;
+	// Get the custom table name.
+	$table_name = $wpdb->prefix . 'immersive_numbers_of_shares';
+	// Check if the user with the supplied user ID has already "Saved for later"
+	// the post with the supplied ID.
+	$results = $wpdb->get_results( "SELECT * FROM {$table_name} WHERE article_id = {$post_id} ", OBJECT );
+
+	// If no result was found, it means that there were no shares for the
+	// current post. By the specs we give the post a rand number of shares (25,
+	// 50) and save the data to the DB.
+	if (empty($results)) {
+		$number_of_shares = rand(25, 50);
+		$wpdb->query("INSERT INTO {$table_name} (article_id, number_of_shares) VALUES ({$post_id}, {$number_of_shares})");
+
+		return $number_of_shares;
+	}
+	else {
+		$result = array_pop($results);
+		return $result->number_of_shares;
+	}
+}
