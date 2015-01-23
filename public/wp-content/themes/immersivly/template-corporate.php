b83674d8fddb46c832c514738bc5e78f4045c343
Stan Laurentiu <laurentiu.stan@freshbyteinc.com>
2015-01-23 18:18:22 +0200

    35-corporate: Add new corporate template.
    https://trello.com/c/CQzA0ZAk/35-ui-corporate-page-layout

---
+++ public/wp-content/themes/immersivly/template-corporate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Displays our Corporate Info.
+ *
+ * @package Immersivly
+ *
+ * Template name: Corporate
+ */
+?>
+
+<article class="entry" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+<div class="row">
+	<div class="small-12 medium-10 medium-centered columns">
+		<header class="entry__header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header>
+
+		<div class="entry__content">
+			<div class="entry__excerpt"><?php the_excerpt(); ?></div>
+			<?php the_content(); ?>
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'immersivly' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div>
+
+		<footer class="entry__footer">
+			<?php edit_post_link( __( 'Edit', 'immersivly' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-footer -->
+	</div>
+</div>
+</article>
