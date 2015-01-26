4dc0cc05120e1d9e59c7773060379823f81ea7a9
Stan Laurentiu <laurentiu.stan@freshbyteinc.com>
2015-01-26 15:07:09 +0200

    35-corporate: Change the corporate template.

--- public/wp-content/themes/immersivly/template-corporate.php
+++ public/wp-content/themes/immersivly/template-corporate.php
@@ -6,31 +6,25 @@
  *
  * Template name: Corporate
  */
-?>
 
-<article class="entry" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+get_header(); ?>
 
+<?php get_sidebar('home'); ?>
 
-<div class="row">
-	<div class="small-12 medium-10 medium-centered columns">
-		<header class="entry__header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header>
+<div class="content content--page">
+	<div class="row collapse">
+		<div class="small-12 columns">
+			<main id="main" class="site-main" role="main">
 
-		<div class="entry__content">
-			<div class="entry__excerpt"><?php the_excerpt(); ?></div>
-			<?php the_content(); ?>
-			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'immersivly' ),
-				'after'  => '</div>',
-			) );
-			?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main>
 		</div>
-
-		<footer class="entry__footer">
-			<?php edit_post_link( __( 'Edit', 'immersivly' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-footer -->
 	</div>
+	<?php get_template_part( 'partial-newsletter');?>
+	<?php get_footer(); ?>
 </div>
-</article>
